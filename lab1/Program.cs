54648f434a2bc786a422b5db8859e5ec0dfa7285
Mykhailo Hordei <159878257+MiGer4@users.noreply.github.com>
2024-08-19 12:46:46 +0300

    Add files via upload

---
+++ lab1/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace lab1
+{
+    internal class Program
+    {
+        static void selfishess(float RainProbability,
+            int ForrestRainFeelings,
+            int ForrestSunFeelings,
+            int HomeRainFeelings,
+            int HomeSunFeelings)
+        {
+            float WHome = HomeRainFeelings * RainProbability + HomeSunFeelings * (1 - RainProbability);
+            float WForrest = ForrestRainFeelings * RainProbability + ForrestSunFeelings * (1 - RainProbability);
+
+            Console.WriteLine("W(home): {0}", WHome);
+            Console.WriteLine("W(forrest): {0}", WForrest);
+
+            if (WHome > WForrest)
+            {
+                Console.WriteLine("Stay at home and play Dota2");
+                return;
+            }
+
+            Console.WriteLine("Let's go to the forrest");
+
+
+        }
+        static void Main(string[] args)
+        {
+            float RainProbability = 0.0f;
+            int ForrestRainFeelings;
+            int ForrestSunFeelings;
+            int HomeRainFeelings;
+            int HomeSunFeelings;
+
+            Console.WriteLine("Hello, Input rain probability 0 < p < 1");
+            RainProbability = Single.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input Forrest rain feelings, 1 <= f <= 10");
+            ForrestRainFeelings = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input Forrest sun feelings, 1 <= f <= 10");
+            ForrestSunFeelings = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Input Home rain feelings, 1 <= f <= 10");
+            HomeRainFeelings = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input Home sun feelings, 1 <= f <= 10");
+            HomeSunFeelings = int.Parse(Console.ReadLine());
+
+            selfishess(RainProbability,
+                ForrestRainFeelings,
+                ForrestSunFeelings,
+                HomeRainFeelings,
+                HomeSunFeelings);
+        }
+    }
+}
